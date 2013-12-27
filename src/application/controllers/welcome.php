c9fc73f616878b7e8dd4de247028d8b6d5daa9ee
Jonathan Irvin <djfoxyslpr@gmail.com>
2013-12-27 16:24:05 -0600

    split view into separate files

--- src/application/controllers/welcome.php
+++ src/application/controllers/welcome.php
@@ -19,7 +19,10 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('header');
+		$this->load->view('nav');
+		$this->load->view('home-content');
+		$this->load->view('footer');
 	}
 }
 
---
+++ src/application/views/footer.php
@@ -0,0 +1,7 @@
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="assets/jqui/js/jquery-1.9.1.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
