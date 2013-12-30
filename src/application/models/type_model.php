a58624043fd87249bb806036e9349716a2df9d33
Jonathan Irvin <djfoxyslpr@gmail.com>
2013-12-30 16:13:07 -0600

    updated priorities model
    updated type model

--- src/application/models/type_model.php
+++ src/application/models/type_model.php
@@ -4,4 +4,35 @@ class Type_model extends CI_Model {
     {
         parent::__construct();
     }
+    
+    function addType($name)
+	{
+		$this->db->set('name', $name); 
+		$this->db->insert('bk_types'); 
+	}
+    
+    function getTypes()
+    {
+		$query = $this->db->get('bk_types');		
+		
+		foreach ($query->result() as $row)
+		{			
+			echo $row->name;			
+		}
+	}
+	
+	function updateType($id,$newname)
+	{
+		$data = array(
+		   'type_id' => $id,
+		   'name' => $newname
+        );
+
+		$this->db->update('bk_types', $data)->where('type_id', $id); 
+	}
+	
+	function deleteType($id)
+	{
+		$this->db->delete('bk_types', array('id' => $id))
+	}
 }
