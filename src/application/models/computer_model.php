dc6382c41586a4d756ef6abb60a683890c46c7ad
Jonathan Irvin <djfoxyslpr@gmail.com>
2014-01-03 14:29:05 -0600

    used @zachflower custom codeigniter model to speed things up, this was from https://gist.github.com/zachflower/5528072 and updated all of the models to reflect it

--- src/application/models/computer_model.php
+++ src/application/models/computer_model.php
@@ -4,4 +4,32 @@ class Computer_model extends CI_Model {
     {
         parent::__construct();
     }
+    
+    function addComputer($ticket_id,$customer_id)
+	{
+		$this->db->set('ticket_id', $ticket_id); 
+		$this->db->set('customer_id', $customer_id); 
+		$this->db->insert('bk_computers'); 
+	}
+    
+    function getComputers()
+    {
+		$query = $this->db->get('bk_computers');		
+		
+		foreach ($query->result() as $row)
+		{			
+			echo $row->ticket_id;
+			echo $row->customer_id;
+		}
+	}
+	
+	function updateComputers($data)
+	{
+		$this->db->update('bk_types', $data, array('computer_id' => $id)); 
+	}
+	
+	function deleteComputer($id)
+	{
+		$this->db->delete('bk_types', array('computer_id' => $id))
+	}
 }
--- src/application/models/customer_model.php
+++ src/application/models/customer_model.php
@@ -4,4 +4,35 @@ class Customer_model extends CI_Model {
     {
         parent::__construct();
     }
+    
+    function addCustomer($name)
+	{
+		$this->db->set('name', $name); 
+		$this->db->insert('bk_customers'); 
+	}
+    
+    function getCustomers()
+    {
+		$query = $this->db->get('bk_customers');		
+		
+		foreach ($query->result() as $row)
+		{			
+			echo $row->name;			
+		}
+	}
+	
+	function updateCustomer($id,$newname)
+	{
+		$data = array(
+		   'type_id' => $id,
+		   'name' => $newname
+        );
+
+		$this->db->update('bk_customers', $data, array('type_id' => $id)); 
+	}
+	
+	function deleteCustomer($id)
+	{
+		$this->db->delete('bk_customers', array('type_id' => $id))
+	}
 }
