0b1a49a7d3ccbafe4789c698c99f0f7a5e5bb544
Jonathan Irvin <djfoxyslpr@gmail.com>
2014-01-03 11:17:40 -0600

    added statuses model

--- src/application/models/statuses_model.php
+++ src/application/models/statuses_model.php
@@ -4,4 +4,37 @@ class Statuses_model extends CI_Model {
     {
         parent::__construct();
     }
+    
+    function addStatus($name, $description)
+	{
+		$this->db->set('name', $name); 
+		$this->db->set('description', $description); 
+		$this->db->insert('bk_status'); 
+	}
+    
+    function getStatusName()
+    {
+		$query = $this->db->get('bk_status');		
+		
+		foreach ($query->result() as $row)
+		{			
+			echo $row->name;						
+		}
+	}
+	
+	function updateStatus($id,$newname,$newdescription)
+	{
+		$data = array(
+		   'status_id' => $id,
+		   'name' => $newname,
+		   'description' => $newdescription,
+        );
+
+		$this->db->update('bk_status', $data, array('status_id' => $id)); 
+	}
+	
+	function deleteStatus($id)
+	{
+		$this->db->delete('bk_status', array('status_id' => $id))
+	}
 }
