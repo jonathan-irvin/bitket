cf20fa71586d1793b569eb0aa67b50e0fd1a39bc
Jonathan Irvin <djfoxyslpr@gmail.com>
2014-01-09 16:55:53 -0600

    added user ticket section

---
+++ src/application/views/dashboard_user.php
@@ -0,0 +1,240 @@
+<!-- BEGIN CONTENT -->
+	<div class="page-content-wrapper">
+		<div class="page-content">					
+			<!-- BEGIN PAGE HEADER-->
+			<div class="row">
+				<div class="col-md-12">
+					<!-- BEGIN PAGE TITLE & BREADCRUMB-->
+					<h3 class="page-title">
+					Dashboard <small>for Jonathan Irvin</small>
+					</h3>
+					<ul class="page-breadcrumb breadcrumb">
+						<li>
+							<i class="fa fa-home"></i>
+							<a href="index.html">Home</a>
+							<i class="fa fa-angle-right"></i>
+						</li>
+						<li>
+							<a href="#">Dashboard</a>
+						</li>
+						<li class="pull-right">
+							<div id="dashboard-report-range" class="dashboard-date-range tooltips" data-placement="top" data-original-title="Change dashboard date range">
+								<i class="fa fa-calendar"></i>
+								<span>
+								</span>
+								<i class="fa fa-angle-down"></i>
+							</div>
+						</li>
+					</ul>
+					<!-- END PAGE TITLE & BREADCRUMB-->
+				</div>
+			</div>
+			<!-- END PAGE HEADER-->
+			<!-- BEGIN DASHBOARD STATS -->
+			<div class="row">
+				<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+					<div class="dashboard-stat blue">
+						<div class="visual">
+							<i class="fa fa-ticket"></i>
+						</div>
+						<div class="details">
+							<div class="number">
+								 1349
+							</div>
+							<div class="desc">
+								 New
+							</div>
+						</div>
+						<a class="more" href="#">
+						View more <i class="m-icon-swapright m-icon-white"></i>
+						</a>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+					<div class="dashboard-stat green">
+						<div class="visual">
+							<i class="fa fa-user"></i>
+						</div>
+						<div class="details">
+							<div class="number">
+								549
+							</div>
+							<div class="desc">
+								Assigned
+							</div>
+						</div>
+						<a class="more" href="#">
+						View more <i class="m-icon-swapright m-icon-white"></i>
+						</a>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+					<div class="dashboard-stat red">
+						<div class="visual">
+							<i class="fa fa-exclamation-circle"></i>
+						</div>
+						<div class="details">
+							<div class="number">
+								89
+							</div>
+							<div class="desc">
+								High Priority
+							</div>
+						</div>
+						<a class="more" href="#">
+						View more <i class="m-icon-swapright m-icon-white"></i>
+						</a>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+					<div class="dashboard-stat yellow">
+						<div class="visual">
+							<i class="fa fa-clock-o"></i>
+						</div>
+						<div class="details">
+							<div class="number">
+								125
+							</div>
+							<div class="desc">
+								On Hold
+							</div>
+						</div>
+						<a class="more" href="#">
+						View more <i class="m-icon-swapright m-icon-white"></i>
+						</a>
+					</div>
+				</div>
+			</div>
+			<!-- END DASHBOARD STATS -->			
+			<div class="clearfix">
+			</div>
+			<div class="row ">
+				<div class="col-md-12">
+					<!-- BEGIN SAMPLE TABLE PORTLET-->
+					<div class="portlet box blue">
+						<div class="portlet-title">
+							<div class="caption">
+								<i class="fa fa-comments"></i>My Tickets
+							</div>
+							<div class="tools">
+								<a href="javascript:;" class="collapse"></a>
+								<a href="#portlet-config" data-toggle="modal" class="config"></a>
+								<a href="javascript:;" class="reload"></a>
+								<a href="javascript:;" class="remove"></a>
+							</div>
+						</div>
+						<div class="portlet-body">
+							<div class="table-responsive">
+								<table class="table table-striped table-hover">
+								<thead>
+								<tr>
+									<th>
+										Ticket #
+									</th>
+									<th>
+										First Name
+									</th>
+									<th>
+										Last Name
+									</th>
+									<th>
+										Created
+									</th>
+									<th>
+										Status
+									</th>
+								</tr>
+								</thead>
+								<tbody>
+								<tr>
+									<td>
+										1
+									</td>
+									<td>
+										Mark
+									</td>
+									<td>
+										Otto
+									</td>
+									<td>
+										2014 Jan 9
+									</td>
+									<td>
+										<span class="label label-sm label-success">
+											Approved
+										</span>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										2
+									</td>
+									<td>
+										Jacob
+									</td>
+									<td>
+										Nilson
+									</td>
+									<td>
+										2014 Jan 9
+									</td>
+									<td>
+										<span class="label label-sm label-info">
+											New
+										</span>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										3
+									</td>
+									<td>
+										Larry
+									</td>
+									<td>
+										Cooper
+									</td>
+									<td>
+										2014 Jan 9
+									</td>
+									<td>
+										<span class="label label-sm label-warning">
+											Suspended
+										</span>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										4
+									</td>
+									<td>
+										Sandy
+									</td>
+									<td>
+										Lim
+									</td>
+									<td>
+										2014 Jan 9
+									</td>
+									<td>
+										<span class="label label-sm label-danger">
+											Blocked
+										</span>
+									</td>
+								</tr>
+								</tbody>
+								</table>
+							</div>
+						</div>
+					</div>
+					<!-- END SAMPLE TABLE PORTLET-->
+					
+				</div>				
+			</div>
+			<div class="clearfix">
+			</div>
+		</div>
+	</div>
+	<!-- END CONTENT -->
+</div>
+<!-- END CONTAINER -->
