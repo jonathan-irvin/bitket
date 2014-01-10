b3c32e71ffed2550a00633a0b7c83930b8482038
Jonathan Irvin <djfoxyslpr@gmail.com>
2014-01-10 15:00:04 -0600

    added queue section

---
+++ src/application/views/queue.php
@@ -0,0 +1,163 @@
+<!-- BEGIN CONTENT -->
+	<div class="page-content-wrapper">
+		<div class="page-content">					
+			<!-- BEGIN PAGE HEADER-->
+			<div class="row">
+				<div class="col-md-12">
+					<!-- BEGIN PAGE TITLE & BREADCRUMB-->
+					<h3 class="page-title">
+					Queue <small>global</small>
+					</h3>
+					<ul class="page-breadcrumb breadcrumb">
+						<li>
+							<i class="fa fa-home"></i>
+							<a href="index.html">Home</a>
+							<i class="fa fa-angle-right"></i>
+						</li>
+						<li>
+							<a href="#">Queue</a>
+						</li>
+						<li class="pull-right">
+							<div id="dashboard-report-range" class="dashboard-date-range tooltips" data-placement="top" data-original-title="Change dashboard date range">
+								<i class="fa fa-calendar"></i>
+								<span>
+								</span>
+								<i class="fa fa-angle-down"></i>
+							</div>
+						</li>
+					</ul>
+					<!-- END PAGE TITLE & BREADCRUMB-->
+				</div>
+			</div>
+			<!-- END PAGE HEADER-->						
+			<div class="clearfix">
+			</div>
+			<div class="row ">
+				<div class="col-md-12">
+					<!-- BEGIN SAMPLE TABLE PORTLET-->
+					<div class="portlet box blue">
+						<div class="portlet-title">
+							<div class="caption">
+								<i class="fa fa-ticket"></i>All Tickets
+							</div>
+							<div class="tools">
+								<a href="javascript:;" class="collapse"></a>
+								<a href="#portlet-config" data-toggle="modal" class="config"></a>
+								<a href="javascript:;" class="reload"></a>
+								<a href="javascript:;" class="remove"></a>
+							</div>
+						</div>
+						<div class="portlet-body">
+							<div class="table-responsive">
+								<table class="table table-striped table-hover">
+								<thead>
+								<tr>
+									<th>
+										Ticket #
+									</th>
+									<th>
+										First Name
+									</th>
+									<th>
+										Last Name
+									</th>
+									<th>
+										Created
+									</th>
+									<th>
+										Status
+									</th>
+								</tr>
+								</thead>
+								<tbody>
+								<tr>
+									<td>
+										1
+									</td>
+									<td>
+										Mark
+									</td>
+									<td>
+										Otto
+									</td>
+									<td>
+										2014 Jan 9
+									</td>
+									<td>
+										<span class="label label-sm label-success">
+											Approved
+										</span>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										2
+									</td>
+									<td>
+										Jacob
+									</td>
+									<td>
+										Nilson
+									</td>
+									<td>
+										2014 Jan 9
+									</td>
+									<td>
+										<span class="label label-sm label-info">
+											New
+										</span>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										3
+									</td>
+									<td>
+										Larry
+									</td>
+									<td>
+										Cooper
+									</td>
+									<td>
+										2014 Jan 9
+									</td>
+									<td>
+										<span class="label label-sm label-warning">
+											Suspended
+										</span>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										4
+									</td>
+									<td>
+										Sandy
+									</td>
+									<td>
+										Lim
+									</td>
+									<td>
+										2014 Jan 9
+									</td>
+									<td>
+										<span class="label label-sm label-danger">
+											Blocked
+										</span>
+									</td>
+								</tr>
+								</tbody>
+								</table>
+							</div>
+						</div>
+					</div>
+					<!-- END SAMPLE TABLE PORTLET-->					
+				</div>				
+			</div>
+			<div class="clearfix">
+			</div>
+		</div>
+	</div>
+	<!-- END CONTENT -->
+</div>
+<!-- END CONTAINER -->
