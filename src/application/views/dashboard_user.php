84df042a96f95a80b0cc76a62ae26e92fcf55b66
Jonathan Irvin <djfoxyslpr@gmail.com>
2014-01-10 14:59:13 -0600

    updated user dashboard

--- src/application/views/dashboard_user.php
+++ src/application/views/dashboard_user.php
@@ -6,7 +6,7 @@
 				<div class="col-md-12">
 					<!-- BEGIN PAGE TITLE & BREADCRUMB-->
 					<h3 class="page-title">
-					Dashboard <small>for Jonathan Irvin</small>
+					Tickets <small>for Jonathan Irvin</small>
 					</h3>
 					<ul class="page-breadcrumb breadcrumb">
 						<li>
@@ -15,7 +15,7 @@
 							<i class="fa fa-angle-right"></i>
 						</li>
 						<li>
-							<a href="#">Dashboard</a>
+							<a href="#">My Tickets</a>
 						</li>
 						<li class="pull-right">
 							<div id="dashboard-report-range" class="dashboard-date-range tooltips" data-placement="top" data-original-title="Change dashboard date range">
@@ -29,83 +29,7 @@
 					<!-- END PAGE TITLE & BREADCRUMB-->
 				</div>
 			</div>
-			<!-- END PAGE HEADER-->
-			<!-- BEGIN DASHBOARD STATS -->
-			<div class="row">
-				<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-					<div class="dashboard-stat blue">
-						<div class="visual">
-							<i class="fa fa-ticket"></i>
-						</div>
-						<div class="details">
-							<div class="number">
-								 1349
-							</div>
-							<div class="desc">
-								 New
-							</div>
-						</div>
-						<a class="more" href="#">
-						View more <i class="m-icon-swapright m-icon-white"></i>
-						</a>
-					</div>
-				</div>
-				<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-					<div class="dashboard-stat green">
-						<div class="visual">
-							<i class="fa fa-user"></i>
-						</div>
-						<div class="details">
-							<div class="number">
-								549
-							</div>
-							<div class="desc">
-								Assigned
-							</div>
-						</div>
-						<a class="more" href="#">
-						View more <i class="m-icon-swapright m-icon-white"></i>
-						</a>
-					</div>
-				</div>
-				<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-					<div class="dashboard-stat red">
-						<div class="visual">
-							<i class="fa fa-exclamation-circle"></i>
-						</div>
-						<div class="details">
-							<div class="number">
-								89
-							</div>
-							<div class="desc">
-								High Priority
-							</div>
-						</div>
-						<a class="more" href="#">
-						View more <i class="m-icon-swapright m-icon-white"></i>
-						</a>
-					</div>
-				</div>
-				<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-					<div class="dashboard-stat yellow">
-						<div class="visual">
-							<i class="fa fa-clock-o"></i>
-						</div>
-						<div class="details">
-							<div class="number">
-								125
-							</div>
-							<div class="desc">
-								On Hold
-							</div>
-						</div>
-						<a class="more" href="#">
-						View more <i class="m-icon-swapright m-icon-white"></i>
-						</a>
-					</div>
-				</div>
-			</div>
-			<!-- END DASHBOARD STATS -->			
+			<!-- END PAGE HEADER-->						
 			<div class="clearfix">
 			</div>
 			<div class="row ">
@@ -114,7 +38,7 @@
 					<div class="portlet box blue">
 						<div class="portlet-title">
 							<div class="caption">
-								<i class="fa fa-comments"></i>My Tickets
+								<i class="fa fa-ticket"></i>My Tickets
 							</div>
 							<div class="tools">
 								<a href="javascript:;" class="collapse"></a>
@@ -227,8 +151,7 @@
 							</div>
 						</div>
 					</div>
-					<!-- END SAMPLE TABLE PORTLET-->
-					
+					<!-- END SAMPLE TABLE PORTLET-->					
 				</div>				
 			</div>
 			<div class="clearfix">
